7f4cbf6cb754896de2d1e1ace826a4d8948ac8c4
davupls <davidmiltonmclean@gmail.com>
2025-02-20 02:16:25 -0500

    Enhance Navbar component with improved styling; add border radius to the navigation bar and padding to navigation items

--- finance-app/src/components/Navbar.js
+++ finance-app/src/components/Navbar.js
@@ -4,11 +4,11 @@ function Navbar() {
     return (
         <nav>
             <ul>
-                <li><Link to="/"><img src="/assets/images/icon-nav-overview.svg"/></Link></li>
-                <li><Link to="/transactions"><img src="/assets/images/icon-nav-transactions.svg"/></Link></li>
-                <li><Link to="/budgets"><img src="/assets/images/icon-nav-budgets.svg"/></Link></li>
-                <li><Link to="/pots"><img src="/assets/images/icon-nav-pots.svg"/></Link></li>
-                <li><Link to="/recurrring"><img src="/assets/images/icon-nav-recurring-bills.svg"/></Link></li>
+                <li><Link to="/"><img className="nav-btn" src="/assets/images/icon-nav-overview.svg"/></Link></li>
+                <li><Link to="/transactions"><img className="nav-btn" src="/assets/images/icon-nav-transactions.svg"/></Link></li>
+                <li><Link to="/budgets"><img className="nav-btn" src="/assets/images/icon-nav-budgets.svg"/></Link></li>
+                <li><Link to="/pots"><img className="nav-btn" src="/assets/images/icon-nav-pots.svg"/></Link></li>
+                <li><Link to="/recurrring"><img className="nav-btn" src="/assets/images/icon-nav-recurring-bills.svg"/></Link></li>
             </ul>
         </nav>
     );
